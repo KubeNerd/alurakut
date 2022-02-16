13bfab06bb32aadb7725e1c8aebd1e18222acadf
Vinicius Pereira <Vinicius.Pereira@processor.com.br>
2022-02-15 23:20:45 -0300

    Inicializando....

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
-import styled from 'styled-components'
+import MainGrid from "../src/components/MainGrid"
+import Box from "../src/components/Box";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import { AlurakutMenu } from "../src/lib/AluraCommons";
+function ProfileSidebar(props){
+  
+
+  return(
+    <Box>
+      <img src={`http://github.com/${props.githubUser}.png`} style={{ borderRadius:'8px'}} />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'shabazzBr';
+  return (
+    <div>
+    <AlurakutMenu/>
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSidebar githubUser={githubUser}/>
+      </div>
+
+      <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+        <Box>
+          Bem vindo
+        </Box>
+      </div>
+
+      <div className="profileRelationsArea" style={{ gridArea: "profileRelationsArea" }} >
+        <Box >
+          Pessoas da Comunidades
+        </Box>
+        <Box>
+          Comunidades
+        </Box>
+      </div>
+
+    </MainGrid>
+    </div>
+  )
 }
